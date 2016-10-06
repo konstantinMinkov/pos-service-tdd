4aa1026cc74caaa9f001ad49fc6236c0e2eb2674
Kostiantyn_Minkov <zakedystyle@gmail.com>
2016-10-06 17:30:41 +0300

    refactor tests

--- src/main/java/ua/rd/pos/VendingMachine.java
+++ src/main/java/ua/rd/pos/VendingMachine.java
@@ -3,9 +3,6 @@ package ua.rd.pos;
 import java.util.ArrayList;
 import java.util.List;
 
-/**
- * Created by Kostiantyn_Minkov on 10/6/2016.
- */
 public class VendingMachine {
 
     private List<Integer> insertedCoins = new ArrayList<>();
--- src/test/java/ua/rd/pos/VendingMachineTest.java
+++ src/test/java/ua/rd/pos/VendingMachineTest.java
@@ -2,20 +2,13 @@ package ua.rd.pos;
 
 import org.junit.Test;
 
-/**
- * Created by Kostiantyn_Minkov on 10/6/2016.
- */
 public class VendingMachineTest {
 
-    @Test
-    public void createVendingMachine() {
-        VendingMachine machine = new VendingMachine();
-    }
+    private VendingMachine machine = new VendingMachine();
 
     @Test
     public void insertCoin() {
         int coinValue = 25;
-        VendingMachine machine = new VendingMachine();
         machine.insertCoin(coinValue);
     }
 }
