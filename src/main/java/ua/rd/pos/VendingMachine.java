e59aa5cf14ed17241f82e9d187f48f35aec628af
Kostiantyn_Minkov <zakedystyle@gmail.com>
2016-10-06 18:01:30 +0300

    add buy and cancel order feature

--- src/main/java/ua/rd/pos/VendingMachine.java
+++ src/main/java/ua/rd/pos/VendingMachine.java
@@ -1,6 +1,7 @@
 package ua.rd.pos;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class VendingMachine {
@@ -9,5 +10,11 @@ public class VendingMachine {
 
     public void insertCoin(Coin coin) {
         insertedCoins.add(coin);
+    }
+
+    public List<Coin> cancelOrder() {
+        final List<Coin> change = Collections.unmodifiableList(insertedCoins);
+        insertedCoins = new ArrayList<>();
+        return change;
     }
 }
--- src/test/java/ua/rd/pos/VendingMachineTest.java
+++ src/test/java/ua/rd/pos/VendingMachineTest.java
@@ -2,6 +2,13 @@ package ua.rd.pos;
 
 import org.junit.Test;
 
+import java.util.List;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertEquals;
+
 public class VendingMachineTest {
 
     private VendingMachine machine = new VendingMachine();
@@ -9,5 +16,16 @@ public class VendingMachineTest {
     @Test
     public void insertCoin() {
         machine.insertCoin(Coin.TWENTY_FIVE);
+    }
+
+    @Test
+    public void buyAndCancel() {
+        machine.insertCoin(Coin.FIFTY);
+        machine.insertCoin(Coin.ONE);
+
+        final List<Coin> change = machine.cancelOrder();
+
+        assertThat(change, hasItems(Coin.FIFTY, Coin.ONE));
+        assertThat(change.size(), is(2));
     }
 }
