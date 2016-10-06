d5d9f1ac1c79fceb440128d796025c9070b48119
Kostiantyn_Minkov <zakedystyle@gmail.com>
2016-10-06 17:25:42 +0300

    create VendingMachine test

---
+++ src/main/java/ua/rd/pos/VendingMachine.java
@@ -0,0 +1,8 @@
+package ua.rd.pos;
+
+/**
+ * Created by Kostiantyn_Minkov on 10/6/2016.
+ */
+public class VendingMachine {
+
+}
---
+++ src/test/java/ua/rd/pos/VendingMachineTest.java
@@ -0,0 +1,14 @@
+package ua.rd.pos;
+
+import org.junit.Test;
+
+/**
+ * Created by Kostiantyn_Minkov on 10/6/2016.
+ */
+public class VendingMachineTest {
+
+    @Test
+    public void createVendingMachine() {
+        VendingMachine machine = new VendingMachine();
+    }
+}
